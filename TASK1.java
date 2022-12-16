6d2acdc7dd74cf7b2888ec097883d8b65c485f6f
Kersty12 <120265840+Kersty12@users.noreply.github.com>
2022-12-16 17:25:37 +0100

    Add files via upload

---
+++ TASK1.java
@@ -0,0 +1,26 @@
+public class TASK1 {
+        public static void main(String[] args)
+        {
+            int i;
+            int a = 5;
+            char b = 'C';
+            float c = 1.7f;
+            double d = 6.895;
+            byte v = 7;
+            short y = 6347;
+            long q = 546893476;
+            String k = "String";
+            Object[] arr = new Object[7];
+            arr[0] = a;
+            arr[1] = b;
+            arr[2] = c;
+            arr[3] = d;
+            arr[4] = v;
+            arr[5] = y;
+            arr[6] = q;
+            for (i = 0; i < 7; i++)
+            {
+                System.out.println(arr[i]);
+            }
+        }
+    }
